11013e9a15d0e76fe5a9520fb93e7a345f657e35
Olav Ausland Onstad <olavao@uia.no>
2022-06-09 04:47:09 +0200

    'Fix'

--- Game/Assets/Scripts/ScriptableObject/Weapon.cs
+++ Game/Assets/Scripts/ScriptableObject/Weapon.cs
@@ -2,19 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : ScriptableObject
+interface IAttack
+{
+    public void PrimaryAttack();
+    public void ChargeAttack();
+    public void SecondaryAttack();
+}
+
+public enum ClassType
+{
+    Mage,
+    Hunter,
+    Bruiser
+}
+
+public class Weapon : ScriptableObject, IAttack
 {
     public string name;
     public Sprite icon;
+    readonly ClassType type;
 
-    public virtual void PrimaryAttack() 
-    {
-        Debug.Log("Fire!");
-    }
+    public virtual void PrimaryAttack() { }
+    public virtual void ChargeAttack() { }
+    public virtual void SecondaryAttack() { }   
 }
 
-[CreateAssetMenu(menuName = "Weapons/Guns/Submachine")]
-public class SubmachineGun : Weapon
+public class Sword : Weapon
+{
+    [SerializeField]
+    private ClassType type = ClassType.Hunter;
+}
+
+[CreateAssetMenu(menuName = "Weapons/Mage/Book")]
+public class Book : Weapon
+{
+    [SerializeField]
+    private ClassType type = ClassType.Mage;
+}
+    
+[CreateAssetMenu(menuName = "Weapons/Hunter/Bow")]
+public class Bow : Weapon
 {
     public float fireRate;
     public float ammunition;
@@ -22,28 +49,14 @@ public class SubmachineGun : Weapon
     public override void PrimaryAttack() {
         Debug.Log("Shoot!");
     }
+
 }
 
-public class Staff : Weapon
-{
-    public GameObject particles;
-}
-
-[CreateAssetMenu(menuName = "Weapons/Staff/FireStaff")]
-public class FireStaff : Staff
+[CreateAssetMenu(menuName = "Weapons/Mage/MagicStaff")]
+public class MagicStaff : Weapon
 {
     public override void PrimaryAttack()
     {
         Debug.Log("Cast Fire Magic");
-    }
-}
-[CreateAssetMenu(menuName = "Weapons/Staff/IceStaff")]
-public class IceStaff : Staff
-{
-    public float fireRate;
-
-    public override void PrimaryAttack()
-    {
-        Debug.Log("Cast Ice Magic");
     }
 }
