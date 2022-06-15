3df7bc37c44456b0138162d7aebc6f02c18d7039
Olav Ausland Onstad <olavao@uia.no>
2022-06-15 16:08:04 +0200

    Removed Armor Combiner

--- Game/Assets/PlayerEquipment.cs
+++ Game/Assets/PlayerEquipment.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerEquipment : MonoBehaviour
 {
     Texture2D tex;
+    Sprite baseSprite { get { return Resources.Load<Sprite>("Player"); } }
     SpriteRenderer sr { get { return GetComponent<SpriteRenderer>(); } }
-    public Transform equipments { get { return transform.Find("Equipments"); } }
-
+    //public Transform equipment;
+    public List<InventorySlot> equipment;
     /*
     public void Combine(Sprite sprite)
     {
@@ -34,4 +35,35 @@ public class PlayerEquipment : MonoBehaviour
         sr.sprite = Sprite.Create(tex, new Rect(0,0,tex.width,tex.height), new Vector2(0.5f, 0.5f), 35);
     }
     */
+    private void Start(){tex = new Texture2D(sr.sprite.texture.width, sr.sprite.texture.height);}
+    public void Update(){if(Input.GetKeyDown(KeyCode.P)){Combine();}}
+
+    public void Combine()
+    {
+        sr.sprite = baseSprite;
+        foreach(InventorySlot slot in equipment)
+        {
+            if(slot.item == null){continue;}
+            for(int x = 0; x < slot.item.icon.texture.width; x++)
+            {
+                for(int y = 0; y < slot.item.icon.texture.height; y++)
+                {
+                    Color colorA = slot.item.icon.texture.GetPixel(x, y);
+                    Color colorB = sr.sprite.texture.GetPixel(x, y);
+                    //check if color is transparent
+                    if(colorA.a != 0)
+                    {
+                        tex.SetPixel(x, y, colorA);
+                    }else if(colorB.a != 0){
+                        tex.SetPixel(x, y, colorB);
+                    }else{
+                        tex.SetPixel(x, y, Color.clear);
+                    }
+                }
+            }
+            tex.filterMode = FilterMode.Point;
+            tex.Apply();
+            sr.sprite = Sprite.Create(tex, new Rect(0,0,tex.width,tex.height), new Vector2(0.5f, 0.5f), 35);
+        }
+    }
 }
---
+++ Game/Assets/Scripts/ScriptableObject/Equipment.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Default Equipment", menuName = "Equipment/Default", order = 1)]
+public class Equipment : Item
+{
+    public int armor;
+}
