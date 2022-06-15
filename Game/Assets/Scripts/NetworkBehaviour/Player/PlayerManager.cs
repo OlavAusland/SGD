34c8e48edb51451cebf86d6b93001e49f7b1d3df
Odd Mathisen <oddoysteim@uia.no>
2022-06-15 16:13:19 +0200

    :construction: Backup of player MP conversion

---
+++ Game/Assets/Scripts/NetworkBehaviour/Player/PlayerManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(PlayerCombat), typeof(SpriteRenderer))]
+public class PlayerManager : NetworkBehaviour
+{
+    public List<GameObject> inventory;
+    public InventoryManager inventoryManager;
+    SpriteRenderer sr { get { return GetComponent<SpriteRenderer>(); } }
+    
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) { ToggleInventory(); }
+    }
+
+    public void ToggleInventory(){ 
+        foreach(GameObject ui in inventory){
+            ui.SetActive(!ui.activeSelf);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Item")) {
+            if(inventoryManager.AddItem(other.GetComponent<ItemInfo>().item)){
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
